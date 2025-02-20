2eebcfd956bf411e25d152477d24fb4e1dd9f7f5
Yash Shinde <shindeyash054@gmail.com>
2025-02-20 15:02:59 +0530

    committtt_3

--- database/database.py
+++ database/database.py
@@ -1,14 +1,8 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///hotel.db"  # SQLite Database
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 
-# Define the Room Model (Table)
 class Room(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     room_number = db.Column(db.String(10), unique=True, nullable=False)
@@ -17,17 +11,9 @@ class Room(db.Model):
     is_available = db.Column(db.Boolean, default=True)
 
 
-# Define the Booking Model (Table)
 class Booking(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     customer_name = db.Column(db.String(100), nullable=False)
     room_id = db.Column(db.Integer, db.ForeignKey("room.id"), nullable=False)
     check_in = db.Column(db.Date, nullable=False)
     check_out = db.Column(db.Date, nullable=False)
-
-
-# Create the database
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    print("Database and tables created successfully!")
